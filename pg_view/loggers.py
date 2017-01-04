1cd1863726a8f72dc14ae028de16680556f0f5b3
Radek Sieradzki <radek.sieradzki@gmail.com>
2017-01-04 22:21:27 +0100

    Refactored models, added tests

--- pg_view/loggers.py
+++ pg_view/loggers.py
@@ -1,12 +1,24 @@
 import logging
 
-logger = None
+logger = logging.getLogger(__name__)
 
 
 def setup_loggers(options):
     global logger
-    logger = logging.getLogger(__name__)
+
+    _setup_basic_config(options)
     logger.setLevel((logging.INFO if options.verbose else logging.ERROR))
     log_stderr = logging.StreamHandler()
     logger.addHandler(log_stderr)
     return log_stderr
+
+
+def _setup_basic_config(options):
+    if options.log_file:
+        LOG_FILE_NAME = options.log_file
+        # truncate the former logs
+        with open(LOG_FILE_NAME, 'w'):
+            pass
+        logging.basicConfig(format='%(levelname)s: %(asctime)-15s %(message)s', filename=LOG_FILE_NAME)
+    else:
+        logging.basicConfig(format='%(levelname)s: %(asctime)-15s %(message)s')
--- pg_view/view.py
+++ pg_view/view.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import logging
 import os
 import platform
 import sys
@@ -10,19 +9,23 @@ import traceback
 from multiprocessing import JoinableQueue  # for then number of cpus
 from optparse import OptionParser
 
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))  # noqa
+from pg_view.exceptions import NotConnectedError, NoPidConnectionError, DuplicatedConnectionError, \
+    InvalidConnectionParamError
+from pg_view.loggers import setup_loggers
+from pg_view.models.parsers import ProcWorker
+
 from pg_view.models.consumers import DiskCollectorConsumer
-from pg_view.models.parsers import get_postmasters_directories
-from pg_view.models.db_client import build_connection, detect_db_connection_arguments, \
-    establish_user_defined_connection, make_cluster_desc
+from pg_view.models.db_client import make_cluster_desc, DBClient
 from pg_view import loggers
 from pg_view import consts
-from pg_view.models.outputs import CommonOutput, CursesOutput
-from pg_view.utils import get_valid_output_methods, OUTPUT_METHOD, \
+from pg_view.models.outputs import CommonOutput, CursesOutput, get_displayer_by_class
+from pg_view.utils import get_valid_output_methods, OUTPUT_METHOD, validate_autodetected_conn_param, \
     output_method_is_valid, read_configuration, process_single_collector, process_groups
 from pg_view.collectors.memory_collector import MemoryStatCollector
 from pg_view.collectors.partition_collector import PartitionStatCollector, DetachedDiskStatCollector
-from pg_view.collectors.pg_collector import PgstatCollector
+from pg_view.collectors.pg_collector import PgStatCollector
 from pg_view.collectors.system_collector import SystemStatCollector
 from pg_view.collectors.host_collector import HostStatCollector
 
@@ -30,10 +33,10 @@ from pg_view.collectors.host_collector import HostStatCollector
 try:
     import psycopg2
     import psycopg2.extras
-
-    psycopg2_available = True
 except ImportError:
-    psycopg2_available = False
+    print('Unable to import psycopg2 module, please, install it (python-psycopg2). Can not continue')
+    sys.exit(254)
+
 try:
     import curses
 
@@ -41,6 +44,10 @@ try:
 except ImportError:
     print('Unable to import ncurses, curses output will be unavailable')
     curses_available = False
+
+# setup system constants
+output_method = OUTPUT_METHOD.curses
+options = None
 
 
 def parse_args():
@@ -55,7 +62,7 @@ def parse_args():
     parser.add_option('-o', '--output-method', help='send output to the following source', action='store',
                       default=OUTPUT_METHOD.curses, dest='output_method')
     parser.add_option('-V', '--use-version',
-                      help='version of the instance to monitor (in case it can\'t be autodetected)',
+                      help="version of the instance to monitor (in case it can't be autodetected)",
                       action='store', dest='version', type='float')
     parser.add_option('-l', '--log-file', help='direct log output to the file', action='store',
                       dest='log_file')
@@ -76,11 +83,6 @@ def parse_args():
 
     options, args = parser.parse_args()
     return options, args
-
-
-# setup system constants
-output_method = OUTPUT_METHOD.curses
-options = None
 
 
 # execution starts here
@@ -113,11 +115,8 @@ def poll_keys(screen, output):
     return True
 
 
-def do_loop(screen, groups, output_method, collectors, consumer):
-    """ Display output (or pass it through to ncurses) """
-
-    output = None
-    if output_method == OUTPUT_METHOD.curses:
+def get_output(method, screen):
+    if method == OUTPUT_METHOD.curses:
         if screen is None:
             loggers.logger.error('No parent screen is passed to the curses application')
             sys.exit(1)
@@ -129,18 +128,23 @@ def do_loop(screen, groups, output_method, collectors, consumer):
                 sys.exit(1)
     else:
         output = CommonOutput()
+    return output
+
+
+def do_loop(screen, groups, output_method, collectors, consumer):
+    """ Display output (or pass it through to ncurses) """
+
+    output = get_output(output_method, screen)
     while 1:
         # process input:
         consumer.consume()
-        for st in collectors:
+        for collector in collectors:
             if output_method == OUTPUT_METHOD.curses:
                 if not poll_keys(screen, output):
                     # bail out immediately
                     return
-            st.set_units_display(consts.display_units)
-            st.set_ignore_autohide(not consts.autohide_fields)
-            st.set_notrim(consts.notrim)
-            process_single_collector(st)
+
+            process_single_collector(collector, consts.filter_aux)
             if output_method == OUTPUT_METHOD.curses:
                 if not poll_keys(screen, output):
                     return
@@ -151,8 +155,15 @@ def do_loop(screen, groups, output_method, collectors, consumer):
         # clears the screen, so we need to refresh before display to clear the old data.
         if options.clear_screen and output_method != OUTPUT_METHOD.curses:
             output.refresh()
-        for st in collectors:
-            output.display(st.output(output_method))
+        for collector in collectors:
+            displayer = get_displayer_by_class(
+                output_method, collector,
+                show_units=consts.display_units,
+                ignore_autohide=not consts.autohide_fields,
+                notrim=consts.notrim
+            )
+            formatted_data = collector.output(displayer)
+            output.display(formatted_data)
         # in the curses case, refresh shows the data queued by display
         if output_method == OUTPUT_METHOD.curses:
             output.refresh()
@@ -168,13 +179,8 @@ def main():
         print('Non Linux database hosts are not supported at the moment. Can not continue')
         sys.exit(243)
 
-    if not psycopg2_available:
-        print('Unable to import psycopg2 module, please, install it (python-psycopg2). Can not continue')
-        sys.exit(254)
-
     options, args = parse_args()
     consts.TICK_LENGTH = options.tick
-
     output_method = options.output_method
 
     if not output_method_is_valid(output_method):
@@ -184,21 +190,9 @@ def main():
 
     if output_method == OUTPUT_METHOD.curses and not curses_available:
         print('Curses output is selected, but curses are unavailable, falling back to console output')
-        output_method == OUTPUT_METHOD.console
+        output_method = OUTPUT_METHOD.console
 
-    # set basic logging
-    if options.log_file:
-        LOG_FILE_NAME = options.log_file
-
-        # truncate the former logs
-        with open(LOG_FILE_NAME, 'w'):
-            pass
-        logging.basicConfig(format='%(levelname)s: %(asctime)-15s %(message)s', filename=LOG_FILE_NAME)
-    else:
-        logging.basicConfig(format='%(levelname)s: %(asctime)-15s %(message)s')
-
-    log_stderr = loggers.setup_loggers(options)
-
+    log_stderr = setup_loggers(options)
     user_dbname = options.instance
     user_dbver = options.version
     clusters = []
@@ -210,62 +204,73 @@ def main():
             if user_dbname and instance != user_dbname:
                 continue
             # pass already aquired connections to make sure we only list unique clusters.
-            host = config[instance].get('host')
-            port = config[instance].get('port')
-            conn = build_connection(host, port,
-                                    config[instance].get('user'), config[instance].get('dbname'))
+            db_client = DBClient.from_config(config[instance])
+            try:
+                cluster = db_client.establish_user_defined_connection(instance, clusters)
+            except (NotConnectedError, NoPidConnectionError):
+                msg = 'failed to acquire details about the database cluster {0}, the server will be skipped'
+                loggers.logger.error(msg.format(instance))
+            except DuplicatedConnectionError:
+                pass
+            else:
+                clusters.append(cluster)
 
-            if not establish_user_defined_connection(instance, conn, clusters):
-                loggers.logger.error('failed to acquire details about ' +
-                                     'the database cluster {0}, the server will be skipped'.format(instance))
     elif options.host:
-        port = options.port or "5432"
-        # try to connet to the database specified by command-line options
-        conn = build_connection(options.host, options.port, options.username, options.dbname)
+        # try to connect to the database specified by command-line options
         instance = options.instance or "default"
-        if not establish_user_defined_connection(instance, conn, clusters):
+        db_client = DBClient.from_options(options)
+        try:
+            cluster = db_client.establish_user_defined_connection(instance, clusters)
+        except (NotConnectedError, NoPidConnectionError):
             loggers.logger.error("unable to continue with cluster {0}".format(instance))
+        except DuplicatedConnectionError:
+            pass
+        else:
+            clusters.append(cluster)
     else:
         # do autodetection
-        postmasters = get_postmasters_directories()
-
+        postmasters = ProcWorker().get_postmasters_directories()
         # get all PostgreSQL instances
-        for result_work_dir, data in postmasters.items():
-            (ppid, dbver, dbname) = data
+        for result_work_dir, connection_params in postmasters.items():
             # if user requested a specific database name and version - don't try to connect to others
-            if user_dbname:
-                if dbname != user_dbname or not result_work_dir or not ppid:
-                    continue
-                if user_dbver is not None and dbver != user_dbver:
-                    continue
             try:
-                conndata = detect_db_connection_arguments(
-                    result_work_dir, ppid, dbver, options.username, options.dbname)
-                if conndata is None:
-                    continue
-                host = conndata['host']
-                port = conndata['port']
-                conn = build_connection(host, port, options.username, options.dbname)
+                validate_autodetected_conn_param(user_dbname, user_dbver, result_work_dir, connection_params)
+            except InvalidConnectionParamError:
+                continue
+            db_client = DBClient.from_postmasters(
+                result_work_dir, connection_params.pid, connection_params.version, options)
+            if db_client is None:
+                continue
+            conn = db_client.connection_builder.build_connection()
+            try:
                 pgcon = psycopg2.connect(**conn)
             except Exception as e:
                 loggers.logger.error('PostgreSQL exception {0}'.format(e))
                 pgcon = None
             if pgcon:
-                desc = make_cluster_desc(name=dbname, version=dbver, workdir=result_work_dir,
-                                         pid=ppid, pgcon=pgcon, conn=conn)
+                desc = make_cluster_desc(
+                    name=connection_params.dbname,
+                    version=connection_params.version,
+                    workdir=result_work_dir,
+                    pid=connection_params.pid,
+                    pgcon=pgcon,
+                    conn=conn
+                )
                 clusters.append(desc)
+
     collectors = []
     groups = {}
     try:
-        if len(clusters) == 0:
+        if not clusters:
             loggers.logger.error('No suitable PostgreSQL instances detected, exiting...')
-            loggers.logger.error('hint: use -v for details, ' +
-                                 'or specify connection parameters manually in the configuration file (-c)')
+            loggers.logger.error('hint: use -v for details, or specify connection parameters '
+                                 'manually in the configuration file (-c)')
             sys.exit(1)
 
         # initialize the disks stat collector process and create an exchange queue
         q = JoinableQueue(1)
         work_directories = [cl['wd'] for cl in clusters if 'wd' in cl]
+
         collector = DetachedDiskStatCollector(q, work_directories)
         collector.start()
         consumer = DiskCollectorConsumer(q)
@@ -273,13 +278,14 @@ def main():
         collectors.append(HostStatCollector())
         collectors.append(SystemStatCollector())
         collectors.append(MemoryStatCollector())
-        for cl in clusters:
-            part = PartitionStatCollector(cl['name'], cl['ver'], cl['wd'], consumer)
-            pg = PgstatCollector(cl['pgcon'], cl['reconnect'], cl['pid'], cl['name'], cl['ver'], options.pid)
-            groupname = cl['wd']
-            groups[groupname] = {'pg': pg, 'partitions': part}
-            collectors.append(part)
-            collectors.append(pg)
+
+        for cluster in clusters:
+            partition_collector = PartitionStatCollector.from_cluster(cluster, consumer)
+            pg_collector = PgStatCollector.from_cluster(cluster, options.pid)
+
+            groups[cluster['wd']] = {'pg': pg_collector, 'partitions': partition_collector}
+            collectors.append(partition_collector)
+            collectors.append(pg_collector)
 
         # we don't want to mix diagnostics messages with useful output, so we log the former into a file.
         loggers.logger.removeHandler(log_stderr)
